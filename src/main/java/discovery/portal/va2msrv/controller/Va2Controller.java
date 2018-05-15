d4c0fd0542ffa7101d2c6260ff664e58f0194730
Joseph Okharedia <JosephO@discovery.co.za>
2018-05-15 17:59:23 +0200

    Initial commit

--- src/main/java/discovery/portal/va2msrv/controller/Va2Controller.java
+++ src/main/java/discovery/portal/va2msrv/controller/Va2Controller.java
@@ -1,8 +1,15 @@
 package discovery.portal.va2msrv.controller;
 
+import discovery.portal.va2msrv.Va2;
+import discovery.portal.va2msrv.schemas.Request;
+import discovery.portal.va2msrv.schemas.Response;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Va2Controller {
+public class Va2Controller implements Va2 {
 
+    @Override
+    public Response postSend(Request requestbody) throws Exception {
+        return null;
+    }
 }
